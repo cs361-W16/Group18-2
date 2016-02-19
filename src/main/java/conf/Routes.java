23bbf0d5ed2a7416fef10936fb78f767c198d70f
wangjiad <wangjiad@oregonstate.edu>
2016-02-19 13:07:55 -0800

    Make some changes
    change start game button to us deck, add another button for s deck. add
    suit of s deck on suit class, and some small change. see screenshots

--- src/main/java/conf/Routes.java
+++ src/main/java/conf/Routes.java
@@ -32,6 +32,7 @@ public class Routes implements ApplicationRoutes {
         router.GET().route("/AcesUp").with(ApplicationController.class, "acesUp");
 
         router.GET().route("/game").with(ApplicationController.class, "gameGet");
+        router.GET().route("/sgame").with(ApplicationController.class, "sgameGet");
         router.POST().route("/dealGame").with(ApplicationController.class, "dealPost");
         router.POST().route("/moveCard/{columnFrom}/{columnTo}").with(ApplicationController.class, "moveCard");
         router.POST().route("/removeCard/{column}").with(ApplicationController.class, "removeCard");
