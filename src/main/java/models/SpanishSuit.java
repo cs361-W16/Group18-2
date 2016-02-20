9474eeb73d2749261e670c7f4ebfcad6a59036ae
luayalshawi <loay.vb@gmail.com>
2016-02-19 21:44:04 -0800

    Created Enum for Us Suit and Spanish Suit.

---
+++ src/main/java/models/SpanishSuit.java
@@ -0,0 +1,11 @@
+package models;
+
+import java.io.Serializable;
+
+/**
+ * Created by Luay on 2/19/2016.
+ */
+public enum SpanishSuit implements Serializable {
+
+    Coins, Cups,Swords,Clubs
+}
---
+++ src/main/java/models/UsSuit.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.io.Serializable;
+
+/**
+ * Created by Luay on 2/19/2016.
+ */
+public enum UsSuit implements Serializable {
+    Hearts, Spades, Diamonds, Clubs
+}
