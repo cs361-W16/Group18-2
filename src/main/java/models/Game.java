b8b37d7c9849fc899030740fe8dd2784d592dfb4
luayalshawi <loay.vb@gmail.com>
2016-02-19 22:59:02 -0800

    All changes to commit. including all 3 classes, base class and two children. Also, added suit's names for Spanish cards.

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -24,12 +24,7 @@ public class Game {
 
 
     public void buildDeck() {
-        for(int i = 2; i < 15; i++){
-            deck.add(new Card(i,Suit.Clubs));
-            deck.add(new Card(i,Suit.Hearts));
-            deck.add(new Card(i,Suit.Diamonds));
-            deck.add(new Card(i,Suit.Spades));
-        }
+
     }
 
 
