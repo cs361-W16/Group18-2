a77e8edffbb393fcd088b9978da829ac970fca48
luayalshawi <loay.vb@gmail.com>
2016-02-19 21:44:52 -0800

    Created a child USgame Class.

---
+++ src/main/java/models/USgame.java
@@ -0,0 +1,16 @@
+package models;
+import models.Game;
+/**
+ * Created by Luay on 2/19/2016.
+ */
+public class USgame extends Game {
+    public void buildDeck() {
+        super.buildDeck();
+        for(int i = 2; i < 15; i++){
+            deck.add(new Card(i,Suit.Clubs));
+            deck.add(new Card(i,Suit.Hearts));
+            deck.add(new Card(i,Suit.Diamonds));
+            deck.add(new Card(i,Suit.Spades));
+        }
+    }
+}
